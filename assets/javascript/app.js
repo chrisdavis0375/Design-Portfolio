7ef1be6c09a8911410b304f409621fb3680478f1
Chris Davis <chrisdavis0375@gmail.com>
2020-08-14 13:34:25 -0500

    Navbar funcionality complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,16 @@ function menuToggle() {
   document.getElementById("navMenu").classList.toggle("show");
   document.getElementById("burger").classList.toggle("nav-button-color");
 }
+window.onclick = function (event) {
+  if (!event.target.matches(".burger")) {
+    var dropdowns = document.getElementsByClassName("nav-menu");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+        document.getElementById("burger").classList.toggle("nav-button-color");
+      }
+    }
+  }
+};
