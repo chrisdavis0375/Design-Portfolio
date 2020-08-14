89bd64dca8e382133633b380520b5b89641a6514
Chris Davis <chrisdavis0375@gmail.com>
2020-08-13 20:18:06 -0500

    Mobile navbar started

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+function menuToggle() {
+  console.log("Menu toggled.");
+  document.getElementById("navMenu").classList.toggle("show");
+  document.getElementById("burger").classList.toggle("nav-button-color");
+}
